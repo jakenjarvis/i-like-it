53725f930bb27177cf21cdf2cc509f97c9e50da0
Jaken.Jarvis <Jaken.Jarvis@c34bea78-e563-11de-b362-3513cf04e977>
2010-02-03 06:27:48 +0000

    サブタイトルの修正。 ローカライズの仕組み導入。（一時的な対応）

---
+++ ILikeIt_localize.js
@@ -0,0 +1,31 @@
+// JSON Object.
+
+var localize =
+{
+	"css":
+	{
+		"en":
+		{
+			"font-family" : "'メイリオ','Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3','ＭＳ Ｐゴシック', 'Osaka', sans-selif;",
+		},
+		"ja":
+		{
+			"font-family" : "'メイリオ','Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3','ＭＳ Ｐゴシック', 'Osaka', sans-selif;",
+		}
+	},
+	"Message":
+	{
+		"en":
+		{
+			"TitleHader" : "fav: ",
+			"LikeMessage" : "I like it!",
+			"DislikeMessage" : "dislike."
+		},
+		"ja":
+		{
+			"TitleHader" : "イイネ: ",
+			"LikeMessage" : "イイネする",
+			"DislikeMessage" : "イイネを取り消す"
+		}
+	},
+}
